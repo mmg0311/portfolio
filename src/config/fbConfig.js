e3c8ffc1fe4b9dd5f1dec2443e435422e789d808
mmg0311 <mohakgadge@gmail.com>
2021-08-01 20:04:19 +0530

    added env variables

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const config = {
-   apiKey: "AIzaSyCEkPdN8R7AlOg9RG9uIwrxqcOrBXZS8Js",
-   authDomain: "mohak-gadge.firebaseapp.com",
-   databaseURL: "https://mohak-gadge-default-rtdb.firebaseio.com",
-   projectId: "mohak-gadge", 
-   storageBucket: "mohak-gadge.appspot.com",
-   messagingSenderId: "809114850502",
-   appId: "1:809114850502:web:b8f1d925ef7370f3aef3c7",
-   measurementId: "G-Z0DF53FKTL"
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: "mohak-gadge.firebaseapp.com",
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: "mohak-gadge",
+  storageBucket: "mohak-gadge.appspot.com",
+  messagingSenderId: process.env.FIREBASE_SENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
 };
 
 firebase.initializeApp(config);
